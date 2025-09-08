30df0c88b8e04e4e07fda9798381870b4bc36ff7
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-09-09 01:47:36 +0800

    Add project files.

---
+++ MileageCorrectFinal/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace MileageCorrectFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRepair = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ALL Table";
+            // 
+            // btnRepair
+            // 
+            this.btnRepair.Location = new System.Drawing.Point(172, 44);
+            this.btnRepair.Name = "btnRepair";
+            this.btnRepair.Size = new System.Drawing.Size(75, 37);
+            this.btnRepair.TabIndex = 8;
+            this.btnRepair.Text = "Repair";
+            this.btnRepair.UseVisualStyleBackColor = true;
+            this.btnRepair.Click += new System.EventHandler(this.btnRepair_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(118, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Final";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(281, 117);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRepair);
+            this.Name = "Form1";
+            this.Text = "Final Mileage - 18 Apr 2017";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRepair;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MileageCorrectFinal/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MileageCorrectFinal
+{
+    public partial class Form1 : Form
+    {
+        string conStr = ConfigurationManager.ConnectionStrings["GPSDBConnectionString"].ConnectionString;
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader rd;
+
+        SqlConnection con1;
+        SqlCommand cmd1;
+        SqlDataReader rd1;
+
+        SqlConnection con2;
+        SqlCommand cmd2;
+        SqlDataReader rd2;
+
+        SqlConnection con3;
+        SqlCommand cmd3;
+        SqlDataReader rd3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLastNid_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FillCombo()
+        {
+        }
+
+        private void cmbTblName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+
+        private void btnRepair_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(conStr);
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = "select TableName, [nTimeFrom] from [All_Track] where [Status] = 0 and nTimeFrom > 0";
+            rd = cmd.ExecuteReader();
+            int nTimeFrom = 0;
+            string tableName;
+            while (rd.Read())
+            {
+                tableName = rd.GetString(0);
+                nTimeFrom = rd.GetInt32(1);
+                DateTime dtFrom = UnixTimeStampToDateTime(Convert.ToDouble(nTimeFrom));
+
+                //zero lat lon
+                con1 = new SqlConnection(conStr);
+                cmd1 = new SqlCommand();
+                cmd1.Connection = con1;
+                con1.Open();
+                cmd1.CommandText = "delete from " + tableName.Trim() + " where dbLat <1 or dbLon <1";
+                cmd1.ExecuteNonQuery();
+                con1.Close();
+
+                con2 = new SqlConnection(conStr);
+                cmd2 = new SqlCommand();
+                cmd2.Connection = con2;
+                con2.Open();
+                cmd2.CommandText = "select nTime, dbLon, dbLat, nMileage from " + tableName + " WHERE nTime BETWEEN DATEDIFF (SECOND, '1970-01-01 06:00:00', '" + dtFrom + "') AND DATEDIFF(SECOND, '1970-01-01 06:00:00', GETDATE()) ORDER BY nTime";
+                rd2 = cmd2.ExecuteReader();
+
+                int i = 0;
+                decimal lon = 0;
+                decimal lat = 0;
+                int m = 0;
+                int calMile;
+                int nTime = 0;
+
+                while (rd2.Read())
+                {
+                    i++;
+                    if (i == 1)
+                    {
+                        lon = rd2.GetDecimal(1);
+                        lat = rd2.GetDecimal(2);
+                        m = rd2.GetInt32(3);
+                    }
+                    else if (i > 1)
+                    {
+                        calMile = Convert.ToInt32(DirectDistance(Convert.ToDouble(lat), Convert.ToDouble(lon), Convert.ToDouble(rd2.GetDecimal(2)), Convert.ToDouble(rd2.GetDecimal(1))));
+                        if (calMile > 80000)
+                        {
+                            calMile = 100;
+                        }
+                        calMile = calMile + m;
+                        nTime = rd2.GetInt32(0);
+
+                        con3 = new SqlConnection(conStr);
+                        cmd3 = new SqlCommand();
+                        cmd3.Connection = con3;
+                        con3.Open();
+                        cmd3.CommandText = "update " + tableName + " set nMileage = " + calMile + " where nTime = " + nTime + "";
+                        cmd3.ExecuteNonQuery();
+                        con3.Close();
+
+                        lon = rd2.GetDecimal(1);
+                        lat = rd2.GetDecimal(2);
+                        m = calMile;
+                    }
+                }
+
+                rd2.Close();
+                con2.Close();
+
+                //update track_last table
+                string strTeid = tableName;
+                if (strTeid.Contains("Track__"))
+                {
+                    strTeid = strTeid.Replace("Track__", "-");
+                }
+                else
+                {
+                    strTeid = strTeid.Replace("Track_", "");
+                }
+
+                con2 = new SqlConnection(conStr);
+                cmd2 = new SqlCommand();
+                cmd2.Connection = con2;
+                con2.Open();
+                cmd2.CommandText = "update Table_TrackLast set nMileage = " + m + " where strTEID = '" + strTeid + "'";
+                cmd2.ExecuteNonQuery();
+                con2.Close();
+
+                con2 = new SqlConnection(conStr);
+                cmd2 = new SqlCommand();
+                cmd2.Connection = con2;
+                con2.Open();
+                cmd2.CommandText = "update [All_Track] set [Status] = 1, [nTimeTo] = " + nTime + " where TableName = '" + tableName + "'";
+                cmd2.ExecuteNonQuery();
+                con2.Close();
+
+                //lblStatus.Text = "SUCCESS! action completed.";
+                
+                //FillCombo();
+            }
+            rd.Close();
+            con.Close();
+            MessageBox.Show("SUCCESS! action completed.");
+        }
+
+        double DirectDistance(double lat1, double lng1, double lat2, double lng2)
+        {
+            double earthRadius = 3959.00;
+            double dLat = ToRadians(lat2 - lat1);
+            double dLng = ToRadians(lng2 - lng1);
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                       Math.Cos(ToRadians(lat1)) * Math.Cos(ToRadians(lat2)) *
+                       Math.Sin(dLng / 2) * Math.Sin(dLng / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double dist = earthRadius * c;
+            double meterConversion = 1609.334;
+            return Math.Ceiling(dist * meterConversion);
+        }
+
+        double ToRadians(double degrees)
+        {
+            double radians = degrees * 3.14159265 / 180;
+            return radians;
+        }
+    }
+}
